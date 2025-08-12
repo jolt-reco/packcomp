091a57dbc5a3dfd92f228991af08f8d3aac93661
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-08-12 22:49:41 +0900

    add: DB接続確認追加&gitignoreへ.pcyファイル追加

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,23 @@
+import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+
+# .env読み込み
+load_dotenv()
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    # config.pyの設定読み込み
+    app.config.from_object("config.Config")
+    
+    # DB接続初期化
+    db.init_app(app)
+
+    # Blueprint登録
     from .routes import bp as routes_bp
     app.register_blueprint(routes_bp)
 
---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+# .env読み込み
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret")
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///dev.db')
