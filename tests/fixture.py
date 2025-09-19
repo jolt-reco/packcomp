ff8322d29bc7a66f937136283b2aea000b7fda15
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-09-19 16:03:03 +0900

    add: pytest用fixtureファイルを追加作成

---
+++ tests/fixture.py
@@ -0,0 +1,16 @@
+import pytest
+from app import create_app, db, models
+
+@pytest.fixture
+def app():
+    app = create_app("testing")
+    with app.app_context():
+        db.create_all()
+        yield app
+        db.session.remove()
+        db.drop_all()
+
+@pytest.fixture
+def session(app):
+    with app.app_context():
+        yield db.session
