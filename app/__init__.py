9a70b229d5b0584799513a2b7f37f5580770b954
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-09-26 17:10:42 +0900

    add: コメント追加

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,11 @@ migrate = Migrate()
 
 def create_app(config_name="default"):
     app = Flask(__name__)
+    # テスト用app使用する場合
     if config_name == "testing":
         from config import TestingConfig
         app.config.from_object(TestingConfig)
+    # 本番用app使用する場合
     else:
         from config import Config
         app.config.from_object(Config)
