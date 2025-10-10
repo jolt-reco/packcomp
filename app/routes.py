b8b3723addc4cd42b41bffafef00ab3243688429
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-10-10 23:05:50 +0900

    add: item_list.html用のルートコード追加(テスト用のため仮データを持たせている)

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 bp = Blueprint("main", __name__)
 
@@ -9,3 +9,26 @@ def index():
 @bp.route("/form")
 def travel_form():
     return render_template("new_travel.html")
+
+@bp.route("/list")
+def items():
+    # travel情報の受信(テスト用のみの記述:削除予定)
+    travel_title = request.form.get("title")
+    destination = request.form.get("destination")
+    male_count = request.form.get("male_count")
+    female_count = request.form.get("female_count")
+    child_count = request.form.get("child_count")
+
+    # 仮アイテムの受信(削除予定)
+    mock_items = [
+        {"name": "歯ブラシ", "quantity": 1},
+        {"name": "パスポート", "quantity": 1},
+        {"name": "オムツ", "quantity": child_count},
+    ]
+
+    return render_template(
+        "item_list.html",
+        title=travel_title,
+        destination=destination,
+        items=mock_items
+    )
