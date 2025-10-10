60e852cfc9916ce363e93ba5a435cf3e86fc53fa
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-10-10 20:35:53 +0900

    fix: エンドポイントをindex→main.indexに変更

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
-def hello():
-    return "hello"
+def index():
+    return render_template("index.html")
+
+@bp.route("/form")
+def travel_form():
+    return render_template("form.html")
+
+@bp.route("/list")
+def item_list():
+    return render_template("list.html")
