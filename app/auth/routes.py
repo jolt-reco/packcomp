a9fc81534459d80bd06822ae58a3426008028e8e
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-10-15 18:22:06 +0900

    fix: render_template()のアクセスファイルパス情報不足のため修正

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -3,8 +3,8 @@ from app.auth import auth_bp
 
 @auth_bp.route("/login")
 def login():
-    return render_template("login.html")
+    return render_template("auth/login.html")
 
 @auth_bp.route("/register")
 def register():
-    return render_template("register.html")
+    return render_template("auth/register.html")
