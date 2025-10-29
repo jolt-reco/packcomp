6038aa0ea3125d011d9edabf45e2faf792cfe7e9
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-10-29 23:01:36 +0900

    app: login_managerの初期化処理追加&user_loaderのデコレーター追加

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 db = SQLAlchemy()
 migrate = Migrate()
+login_manager = LoginManager()
 
 def create_app(config_name="default"):
     app = Flask(__name__)
@@ -19,6 +21,7 @@ def create_app(config_name="default"):
     # DB接続初期化
     db.init_app(app)
     migrate.init_app(app, db)
+    login_manager.init_app(app)
 
     # Blueprint登録
     from app.main import main_bp
@@ -26,6 +29,15 @@ def create_app(config_name="default"):
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
 
+    # 未ログインユーザーをログインページへリダイレクト
+    login_manager.login_view = "auth.login"
+
     from . import models
 
     return app
+
+from app import db
+from app.models import User
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
