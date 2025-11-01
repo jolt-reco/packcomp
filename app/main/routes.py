c7ce941acba6b373dd825e09d297cb36554af493
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-11-01 12:16:58 +0900

    update: items以下インデント修正

--- app/main/routes.py
+++ app/main/routes.py
@@ -57,25 +57,25 @@ def new_travel():
 @main_bp.route("/list", methods=["GET", "POST"])
 def items():
     # travel情報の受信(テスト用のみの記述:削除予定)
-        travel_title = request.form.get("title")
-        destination = request.form.get("destination")
-        male_count = request.form.get("male_count")
-        female_count = request.form.get("female_count")
-        child_count = request.form.get("child_count")
+    travel_title = request.form.get("title")
+    destination = request.form.get("destination")
+    male_count = request.form.get("male_count")
+    female_count = request.form.get("female_count")
+    child_count = request.form.get("child_count")
 
     # 仮アイテムの受信(削除予定)
-        mock_items = [
-            {"name": "歯ブラシ", "quantity": 1},
-            {"name": "パスポート", "quantity": 1},
-            {"name": "オムツ", "quantity": child_count},
-        ]
+    mock_items = [
+        {"name": "歯ブラシ", "quantity": 1},
+        {"name": "パスポート", "quantity": 1},
+        {"name": "オムツ", "quantity": child_count},
+    ]
 
-        return render_template(
-            "items_list.html",
-            title=travel_title,
-            destination=destination,
-            items=mock_items
-        )
+    return render_template(
+        "items_list.html",
+        title=travel_title,
+        destination=destination,
+        items=mock_items
+    )
 
 @main_bp.route("/travel/<int:travel_id>/select_purpose", methods=["GET", "POST"])
 @login_required
