cbd1803d4c7cb39da352f0f666a52e8ebad37e56
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-11-02 12:37:07 +0900

    add: travels_list&new_travelからのtravel.idの受取&travel情報のget追加

--- app/main/routes.py
+++ app/main/routes.py
@@ -46,7 +46,7 @@ def new_travel():
             db.session.add(new_travel)
             db.session.commit()
             flash("旅行を登録しました！", "success")
-            return redirect(url_for("main.items"))
+            return redirect(url_for("main.items", travel_id=new_travel.id))
         
         except Exception as e:
             db.session.rollback()
@@ -54,26 +54,19 @@ def new_travel():
 
     return render_template("new_travel.html")
 
-@main_bp.route("/list", methods=["GET", "POST"])
-def items():
-    # travel情報の受信(テスト用のみの記述:削除予定)
-    travel_title = request.form.get("title")
-    destination = request.form.get("destination")
-    male_count = request.form.get("male_count")
-    female_count = request.form.get("female_count")
-    child_count = request.form.get("child_count")
+@main_bp.route("/list/<int:travel_id>", methods=["GET", "POST"])
+def items(travel_id):
+    travel = Travel.query.get_or_404(travel_id)
 
     # 仮アイテムの受信(削除予定)
     mock_items = [
         {"name": "歯ブラシ", "quantity": 1},
         {"name": "パスポート", "quantity": 1},
-        {"name": "オムツ", "quantity": child_count},
     ]
 
     return render_template(
         "items_list.html",
-        title=travel_title,
-        destination=destination,
+        travel=travel,
         items=mock_items
     )
 
