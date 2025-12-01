c2acf940475b096dc438048d788bbb02f6800711
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-12-01 12:55:47 +0900

    add: open weather forcastエンドポイント&API KEYの追加

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret")
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///dev.db')
+    WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+    WEATHER_FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast" 
 
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
