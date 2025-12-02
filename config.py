0af460a4c072434f9a8b3981866e46a84f2be597
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-12-02 14:34:48 +0900

    add: OpenMeteo,NomitatimURIの追加

--- config.py
+++ config.py
@@ -7,6 +7,8 @@ load_dotenv()
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "dev_secret")
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///dev.db')
+    NOMINATIM_URI = ("https://nominatim.openstreetmap.org/search")
+    OPENMETEO_URI = ("https://api.open-meteo.com/v1/forecast")
 
 class TestingConfig(Config):
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
