c8bd20039b4210f168a06815fa9bbf9a88bb884b
Rikuto Fukuda <fukuriku1205@gmail.com>
2025-12-17 16:54:50 +0900

    fix: 起動時のDBテーブル生成処理を追加(初回のみ)

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
 from app import create_app
+from flask_migrate import upgrade
+from app.seeds import ensure_seed_data
 
 app = create_app()
+
+with app.app_context():
+    upgrade()
+    ensure_seed_data()
 
 if __name__ == "__main__":
     app.run(port=8000)
